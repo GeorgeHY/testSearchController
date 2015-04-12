307c0ae5b7adccf137275d1e4040b731f81ae12b
GeorgeHan <hanyangkid@163.com>
2015-04-12 09:16:09 +0800

    Initial Commit

---
+++ testSearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testSearchBar
+//
+//  Created by iwind on 15/4/12.
+//  Copyright (c) 2015年 HanYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testSearchBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testSearchBar
+//
+//  Created by iwind on 15/4/12.
+//  Copyright (c) 2015年 HanYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
